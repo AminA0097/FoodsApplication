c36f218a066e13b2a394974472da05c36ead66f3
AminA0097 <springmcserv@gmailc.om>
2025-03-29 14:15:46 +0330

    FoodsApplication

--- src/main/java/com/springsecurity/foods/Foods/FoodsController.java
+++ src/main/java/com/springsecurity/foods/Foods/FoodsController.java
@@ -1,26 +1,22 @@
 package com.springsecurity.foods.Foods;
-import org.springframework.stereotype.Controller;
+import com.springsecurity.foods.Bases.Response;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("foods/")
-@Controller
+@RestController
 public class FoodsController {
     private final FoodsInterface foodsInterface;
     public FoodsController(FoodsInterface foodsInterface) {
         this.foodsInterface = foodsInterface;
     }
-    @PostMapping("/post")
+    @PostMapping("/addnew")
     public String addFood(@RequestBody FoodsForm foodsForm)throws Exception{
         foodsInterface.save(foodsForm);
-        return "Foods/Foods";
+        return "Successfully added food!";
     }
     @GetMapping("/getfoods/{pageNumb}")
     public String getFoods(@PathVariable("pageNumb")Integer pageNumb ) throws Exception {
         foodsInterface.findAll(5,pageNumb);
-        return "Foods/Foods";
-    }
-    @GetMapping("/addnew")
-    public String addNewFood()throws Exception{
-        return "Foods/Foods";
+        return "Foods";
     }
 }
